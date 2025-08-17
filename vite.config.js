eaec7254a355f4f5c177e7a38b2fb2eb480e93b1
Alexey Korepanov <alexey.korepanov@clickhouse.com>
2025-08-17 17:09:06 +0200

    Change base URL to /virus.exe/ to fix github pages problem

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Ensure assets are served from the repository subpath on GitHub Pages
+  // and locally available at http://localhost:5173/virus.exe/
+  base: '/virus.exe/',
   plugins: [react()],
 });
